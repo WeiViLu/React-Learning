701d035aef111e7bd5066a0865f17e9fed3ac46b
陆薇薇 <WeiViLu@outlook.com>
2018-01-19 14:23:50 +0800

    IndexRoute
    默认路由

---
+++ 中级阶段/react-router-learning/IndexRoute/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {render} from 'react-dom'
+import {Router,Route,hashHistory,IndexRoute} from 'react-router'
+
+import App from './modules/App.js'
+import About from './modules/About.js'
+import Repos from './modules/Repos.js'
+import Repo from './modules/Repo.js'
+import Home from './modules/Home.js'
+
+render((<Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/repos" component={Repos}>
+                <Route path="/repos/:userName/:reposName" component={Repo}/>
+            </Route>
+        </Route>
+    </Router>),document.getElementById('app'));
+
