9ff6938b362f8146c71de084bc3c1b09502dd82e
陆薇薇 <WeiViLu@outlook.com>
2018-02-02 15:24:39 +0800

    dianping-city

---
+++ 中级阶段/react-dianping/app/components/Header/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+
+import './style.less'
+
+class Header extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    }
+
+    clickHandler(){
+        window.history.back();
+    }
+
+    render() {
+        return (
+            <div id="common-header">
+                <span className="back-icon"  onClick={this.clickHandler.bind(this)}>
+                    <i className="icon-chevron-left"></i>
+                </span>
+                <h1>{this.props.title}</h1>
+            </div>
+        )
+    }
+}
+
+export default Header
--- 中级阶段/react-dianping/app/containers/City/index.jsx
+++ 中级阶段/react-dianping/app/containers/City/index.jsx
@@ -1,20 +1,61 @@
-import React from 'react'
-import PureRenderMixin from 'react-addons-pure-render-mixin'
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import * as userInfoActionsFromOtherFile from '../../actions/userinfo';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import LocalStore from '../../util/localStore'
+import {CITYNAME} from '../../config/localStoreKey'
+import { hashHistory } from 'react-router'
+
+
+import Header from '../../components/Header'
+import CurrentCity from '../../components/CurrentCity'
+import CityList from '../../components/CityList'
 
 class City extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
     }
+
+    componentDidMount(){
+    }
+
+    changeNewCity(newCity){
+        if(newCity == null){
+            return
+        }
+
+        //修改redux
+        this.props.userinfo.cityName=newCity;
+        this.props.userInfoActions.update(this.props.userinfo);
+
+        //修改localstorage
+        LocalStore.setItem(CITYNAME,newCity);
+
+        //返回首页
+        hashHistory.push('/');
+    }
+
     render() {
         return (
             <div>
-                <h1>city</h1>
+               <Header title="选择城市"/>
+               <CurrentCity cityName={this.props.userinfo.cityName}/>
+               <CityList  changeFn={this.changeNewCity.bind(this)}/>
             </div>
         )
     }
 }
 
-// 使用 require.ensure 异步加载，还不支持 ES6 的 export 
-// export default City
-module.exports = City
+function mapStateToProps(state){
+    return{ userinfo: state.userinfo }
+}
+
+function mapDispatchToProps(dispatch){
+    return{
+        userInfoActions: bindActionCreators(userInfoActionsFromOtherFile,dispatch)
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(City)
