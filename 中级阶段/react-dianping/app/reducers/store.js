6c479746a669ffe95e81c0400e1ef812d70a873b
陆薇薇 <WeiViLu@outlook.com>
2018-02-08 07:58:39 +0800

    dianping-buy modify
    修改buy的错误

--- 中级阶段/react-dianping/app/reducers/store.js
+++ 中级阶段/react-dianping/app/reducers/store.js
@@ -3,9 +3,9 @@ import * as actionTypes from '../constants/store'
 const initialState=[]
 
 export default function store (state = initialState, action){
-    switch(actionTypes.Type){
+    switch (action.type) {
         case actionTypes.STORE_UPDATE:
-            return actionTypes.data
+            return action.data
         case actionTypes.STORE_ADD:
             state.unshift(action.data)
             return state
