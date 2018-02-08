d029b19e4992d26dd4701f2b2cd271a348c494c0
陆薇薇 <WeiViLu@outlook.com>
2018-02-08 17:55:32 +0800

    dianping-usercomment

--- 中级阶段/react-dianping/app/components/OrderList/Item/index.jsx
+++ 中级阶段/react-dianping/app/components/OrderList/Item/index.jsx
@@ -7,7 +7,33 @@ class Item extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state={
+            commentState: 2
+        }
     }
+
+    componentDidMount(){
+        this.setState({
+            commentState: this.props.data.commentState
+        })
+    }
+
+    showComment(){
+        this.setState({
+            commentState: 1
+        })
+    }
+    //取消按钮
+    hideComment(){
+        this.setState({
+            commentState: 0
+        })
+    }
+    //提交按钮
+    submitComment(){
+
+    }
+
     render() {
         const data = this.props.data;
         return (
@@ -16,13 +42,31 @@ class Item extends React.Component {
                     <img src={data.img}/>
                 </div>
                 <div className="order-item-comment float-right">
-                    <button>评价</button>
+                   {
+                       //未评价
+                       this.state.commentState === 0
+                       ? <button className="btn" onClick={this.showComment.bind(this)}>评价</button>
+                        // 评价中
+                        : this.state.commentState === 1
+                        ? ''
+                          : <button className="btn unselected-btn">已评价</button>
+                   }
                 </div>
                 <div className="order-item-content">
                     <span>商户：{data.title}</span>
                     <span>数量：{data.count}</span>
                     <span>价格：￥{data.price}</span>
                 </div>
+                {
+                    this.state.commentState === 1
+                    ? <div className="comment-text-container">
+                        <textarea style={{width: '100%', height: '80px'}} className="comment-text" ref="commentText"></textarea>
+                        <button className="btn" onClick={this.submitComment.bind(this)}>提交</button>
+                        &nbsp;
+                        <button className="btn unselected-btn" onClick={this.hideComment.bind(this)}>取消</button>
+                    </div>
+                    : ''
+                }
             </div>
         )
     }
